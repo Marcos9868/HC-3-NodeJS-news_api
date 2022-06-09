b8a26bbdc26b26b79a193e31e1fc0cad6bb44768
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-06-09 15:16:28 -0300

    Centraliza todos os módulos da news-api

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// Importação dos módulos
+const express = require('express')
+const statusRoute = require('./routes/status.route')
+const categoryNews = require('./routes/category.news.route')
+const newsRoute = require('./routes/news.route')
+
+// Innstância Express
+const app = express()
+
+// Configurações Gerais
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Rotas
+// Rota Status:
+app.use(statusRoute)
+
+// Rota Categorias:
+app.use(categoryNews)
+
+// Rota Notícias:
+app.use(newsRoute)
+
+app.listen(4000, () => {
+  console.log('Servidor rodando em http://localhost:4000')
+})
